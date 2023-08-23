43510a4ab0062529f73f52901ec5087f78f32643
DangQuyen1923 <dangxuanquyen1923@gamil.com>
2023-08-23 23:04:01 +0700

    portfolio

--- main.js
+++ main.js
@@ -37,3 +37,36 @@ function shadowHeader() {
   else header.classList.remove("shadow-header");
 }
 window.addEventListener("scroll", shadowHeader);
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_fvbhdjl",
+      "template_1h3nc3p",
+      "#contact-form",
+      "N8Vv7Xx31YCTMDyeS"
+    )
+    .then(
+      () => {
+        contactMessage.textContent = "Tin nhắn đã được gửi thành công ✅";
+
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 3000);
+
+        contactForm.reset();
+      },
+      () => {
+        contactMessage.textContent = "Tin nhắn không được gửi (lỗi dịch vụ) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
