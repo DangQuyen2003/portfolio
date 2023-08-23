748f3a43c8edaff9e877cc8fc7c07651d43e649a
DangQuyen1923 <dangxuanquyen1923@gamil.com>
2023-08-23 14:29:41 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function shadowHeader() {
+  const header = document.getElementById("header");
+
+  if (this.scrollY >= 50) header.classList.add("shadow-header");
+  else header.classList.remove("shadow-header");
+}
+window.addEventListener("scroll", shadowHeader);
